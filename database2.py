6f3b84408dacabe73c1b1a1e2623ff12c9ebfb06
Yu-Tsung Chiu <lapulato@gmail.comecho>
2022-12-11 21:21:39 +0800

    mysql demo

---
+++ database2.py
@@ -0,0 +1,30 @@
+import mysql.connector
+
+try:
+    # 連接 MySQL/MariaDB 資料庫
+    connection = mysql.connector.connect(
+        host='localhost',          # 主機名稱
+        database='world',          # 資料庫名稱
+        user='root',               # 帳號
+        password='root')           # 密碼
+
+    if connection.is_connected():
+
+        # 顯示資料庫版本
+        db_Info = connection.get_server_info()
+        print("資料庫版本：", db_Info)
+        print(connection)
+        # 顯示目前使用的資料庫
+        cursor = connection.cursor()
+        cursor.execute("SELECT DATABASE();")
+        record = cursor.fetchone()
+        print("目前使用的資料庫：", record)
+
+except Error as e:
+    print("資料庫連接失敗：", e)
+
+finally:
+    if (connection.is_connected()):
+        cursor.close()
+        connection.close()
+        print("資料庫連線已關閉")
